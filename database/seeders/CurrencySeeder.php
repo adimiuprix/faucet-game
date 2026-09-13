8e6ba364a7f55540dbb1c6dfd34e07f73b1b144f
adimiuprix <miuprix@gmail.com>
2026-09-13 19:36:30 +0700

    feat(seeder): Expand cryptocurrency support with 22 new coins
    - Add 22 new cryptocurrency coins to currency seeder (BTC, ETH, DOGE, LTC, BCH, DASH, DGB, ZEC, BNB, XRP, POL, ADA, TON, XLM, USDC, XMR, TRUMP, PEPE, FLT)
    - Reorder existing coins to group by status (inactive coins first, then active coins)
    - Set most new coins to inactive status with default faucet reward of 0.00000010 or 0.00500000
    - Activate TRX and POL coins alongside existing active coins (USDT, FEY, SOL)
    - Add SVG image assets for all new currencies to support faucet interface

--- database/seeders/CurrencySeeder.php
+++ database/seeders/CurrencySeeder.php
@@ -14,10 +14,52 @@ class CurrencySeeder extends Seeder
     {
         $currencies = [
             [
-                'coin' => 'FEY',
+                'coin' => 'BTC',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00000010,
+                'image' => 'btc.svg',
+            ],
+            [
+                'coin' => 'ETH',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00000010,
+                'image' => 'eth.svg',
+            ],
+            [
+                'coin' => 'DOGE',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00000010,
+                'image' => 'doge.svg',
+            ],
+            [
+                'coin' => 'LTC',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00000010,
+                'image' => 'ltc.svg',
+            ],
+            [
+                'coin' => 'BCH',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00000010,
+                'image' => 'bch.svg',
+            ],
+            [
+                'coin' => 'DASH',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00000010,
+                'image' => 'dash.svg',
+            ],
+            [
+                'coin' => 'DGB',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00000010,
+                'image' => 'dgb.svg',
+            ],
+            [
+                'coin' => 'TRX',
                 'status' => 'active',
-                'faucet_reward' => 0.00500000,
-                'image' => 'fey.svg',
+                'faucet_reward' => 0.00000010,
+                'image' => 'trx.svg',
             ],
             [
                 'coin' => 'USDT',
@@ -26,10 +68,88 @@ class CurrencySeeder extends Seeder
                 'image' => 'usdt.svg',
             ],
             [
+                'coin' => 'FEY',
+                'status' => 'active',
+                'faucet_reward' => 0.00500000,
+                'image' => 'fey.svg',
+            ],
+            [
+                'coin' => 'ZEC',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00500000,
+                'image' => 'zec.svg',
+            ],
+            [
+                'coin' => 'BNB',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00500000,
+                'image' => 'bnb.svg',
+            ],
+            [
                 'coin' => 'SOL',
                 'status' => 'active',
                 'faucet_reward' => 0.00500000,
                 'image' => 'sol.svg',
+            ],
+            [
+                'coin' => 'XRP',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00500000,
+                'image' => 'xrp.svg',
+            ],
+            [
+                'coin' => 'POL',
+                'status' => 'active',
+                'faucet_reward' => 0.00500000,
+                'image' => 'pol.svg',
+            ],
+            [
+                'coin' => 'ADA',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00500000,
+                'image' => 'ada.svg',
+            ],
+            [
+                'coin' => 'TON',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00500000,
+                'image' => 'ton.svg',
+            ],
+            [
+                'coin' => 'XLM',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00500000,
+                'image' => 'xlm.svg',
+            ],
+            [
+                'coin' => 'USDC',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00500000,
+                'image' => 'usdc.svg',
+            ],
+            [
+                'coin' => 'XMR',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00500000,
+                'image' => 'xmr.svg',
+            ],
+            [
+                'coin' => 'TRUMP',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00500000,
+                'image' => 'trump.svg',
+            ],
+            [
+                'coin' => 'PEPE',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00500000,
+                'image' => 'pepe.svg',
+            ],
+            [
+                'coin' => 'FLT',
+                'status' => 'inactive',
+                'faucet_reward' => 0.00500000,
+                'image' => 'flt.svg',
             ],
         ];
 
